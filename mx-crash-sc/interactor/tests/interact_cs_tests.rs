972f36f825d6d56dac88c66e39b217c625874be7
Andrei Baltariu <andreibaltariu1304@gmail.com>
2025-07-07 13:44:52 +0300

    Added submit_test cs test

--- mx-crash-sc/interactor/tests/interact_cs_tests.rs
+++ mx-crash-sc/interactor/tests/interact_cs_tests.rs
@@ -6,10 +6,29 @@ use rust_interact::ContractInteract;
 // The chain simulator should already be installed and running before attempting to run this test.
 // The chain-simulator-tests feature should be present in Cargo.toml.
 // Can be run with `sc-meta test -c`.
+// #[tokio::test]
+// #[cfg_attr(not(feature = "chain-simulator-tests"), ignore)]
+// async fn deploy_test_mx_crash_sc_cs() {
+//     let mut interactor = ContractInteract::new().await;
+
+//     interactor.deploy().await;
+//     interactor.new_game().await;
+// }
+
 #[tokio::test]
 #[cfg_attr(not(feature = "chain-simulator-tests"), ignore)]
-async fn deploy_test_mx_crash_sc_cs() {
+async fn submit_bet_test() {
     let mut interactor = ContractInteract::new().await;
 
     interactor.deploy().await;
+
+    let sc_balance = BigUint::from(100_000_000_000u64);
+    interactor.deposit(sc_balance).await;
+
+    interactor.new_game().await;
+
+    let bet = BigUint::from(100_000u64);
+    interactor
+        .submit_bet(test_wallets::alice().to_address(), 10, bet)
+        .await;
 }
